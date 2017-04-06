afef932ba8fa30f376967d715ac3faa6b2241fd8
Joey DeFrancesco <jdefr89@gmail.com>
2017-04-06 15:00:19 -0400

    Cleaning up code

--- armjitsu/armcpu.py
+++ armjitsu/armcpu.py
@@ -13,12 +13,11 @@ from capstone import *
 from misc_utils import *
 
 class AddressOutOfRange(Exception):
+    """AddressOutOfRange Exception for Addresses not valid in code space"""
 
-    def __init__(self, message, errors):
+    def __init__(self, message):
         super(AddressOutOfRange, self).__init__(message)
-
         self.message = message
-        self.errors = errors
 
 
 class ArmCPU(object):
@@ -29,21 +28,41 @@ class ArmCPU(object):
         code (byte array): Actual code to run in emulator.
     """
 
+    # pylint: disable=too-many-instance-attributes
+    # Large class will have quite a few control attributes...
+
 
     def __init__(self, address, code):
         self.break_points = {}
         self.sys_calls = []
 
         self.unique_bp_id = 0
+        self.break_addr = False
 
+        # Our emulator object from unicorn
         self.emu = None
+
         self.was_thumb = False
 
+        # Set code and address variables
+        self.code = code
+        self.start_addr = address
+        self.end_addr = self.start_addr + len(self.code)
+
+        # Variables to control stopping and resuming
+        self.is_running = False
+        self.use_step_mode = False
+        self.stop_now = False
+
+        self.registers = {}
+        self.areas = {}
+        # Original start address
         self.saved_start = address
-        self.emu_init(address, code)
+
+        self.emu_init()
 
 
-    def emu_init(self, address, code):
+    def emu_init(self):
         """emu_init() - called by constructor to setup
         our emulation object.
 
@@ -52,24 +71,11 @@ class ArmCPU(object):
             code(byte array):
 
         """
-
-        self.code = code
-        self.start_addr = address
-        self.end_addr = self.start_addr + len(self.code)
-
-        self.is_running = False
-        self.use_step_mode = False
-        self.stop_now = False
-
-        self.registers = {}
-        self.areas = {}
-
-
         try:
             self.emu = Uc(UC_ARCH_ARM, UC_MODE_ARM)
             self.emu_init_memory()
             self.emu_init_registers()
-        except UcError as e:
+        except UcError as err:
             print "[-] Error setting up!"
             return False
 
@@ -97,14 +103,14 @@ class ArmCPU(object):
 
 
     def run(self):
-        """run() - start emulation. Ensure the emulation object is properly initalized before
+        """run() - start emulation. Ensure the emulation object is properly initalized before,gv
         calling this method.
         """
         try:
             self.is_running = True
             if self.was_thumb: self.start_addr |= 1
             self.emu.emu_start(self.start_addr, self.end_addr)
-        except UcError as e:
+        except UcError as err:
             self.emu.emu_stop()
             return
 
@@ -118,7 +124,6 @@ class ArmCPU(object):
         This method could be thought of as a deconstructor usually called before exiting
         armjitsu.
         """
-        self.emu.mem_unmap(self.start_addr)
         self.emu.emu_stop()
         del self.emu
         self.emu = None
@@ -131,7 +136,10 @@ class ArmCPU(object):
 
         # Dump registers R0 to R9
         for reg in xrange(UC_ARM_REG_R0, UC_ARM_REG_R0 + 10):
-            reg_string = "[*] R{:<3d} = 0x{:08x}".format((reg-UC_ARM_REG_R0), self.emu.reg_read(reg))
+
+            reg_string = "[*] R{:<3d} = 0x{:08x}".format((reg-UC_ARM_REG_R0),
+                                                         self.emu.reg_read(reg))
+
             print reg_string
 
         print ""
@@ -146,6 +154,7 @@ class ArmCPU(object):
         print "[*] CPSR = 0x{:08x}".format(self.emu.reg_read(UC_ARM_REG_CPSR))
         print ""
 
+
     def get_pc(self):
         """get_pc() - returns Program Counter register (PC)."""
         return self.emu.reg_read(UC_ARM_REG_PC)
@@ -159,8 +168,8 @@ class ArmCPU(object):
     def dump_state(self):
         """dump_state() - dumps state of emulation machine.
 
-        This method should dump the context of the emulator. Register values, backtrace, and current instructions executed
-        should all be printed to stdout.
+        This method should dump the context of the emulator. Register values, backtrace,
+        and current instructions executed should all be printed to stdout.
 
         Example:
             >>> inst.dump_state()
@@ -171,7 +180,8 @@ class ArmCPU(object):
 
 
     def set_breakpoint_address(self, break_addr):
-        if  not (self.start_addr <= break_addr <= self.end_addr):
+        """Set a breakpoint by address."""
+        if  not self.start_addr <= break_addr <= self.end_addr:
             raise AddressOutOfRange("Address is out of current range!")
         else:
             if break_addr not in self.break_points.values():
@@ -180,6 +190,7 @@ class ArmCPU(object):
 
 
     def list_breakpoints(self):
+        """List breakpoints."""
         print "Break points:"
         if self.break_points:
             for bp_id, break_addr in self.break_points:
@@ -190,10 +201,13 @@ class ArmCPU(object):
         pass
 
 
-    def main_code_hook(self, uc, address, size, user_data):
-        """ main_code_hook()"""
+    def main_code_hook(self, uc, address, size):
+        """Hooks all code. This will be the hook that handles pausing and resuming
+        execution if say, a break point is hit or the user is stepping through
+        code.
+        """
 
-        # Read the current instruction to execute
+        # Read the instruction that is about to execute
         try:
             mem_tmp = self.emu.mem_read(address, size)
             inst = ["0x{:02x}".format(i) for i in mem_tmp]
@@ -201,30 +215,36 @@ class ArmCPU(object):
 
             out_string = "0x{:08x}: {}\n".format(address, inst_string)
 
-        except UcError as e:
-            print "ERROR: %s", e
+        except UcError as err:
+            print "ERROR: %s", err
 
 
+        # Stop execution if there is a reason to. Eg: break point hit or code stepping
         if self.stop_now:
-             self.start_addr = self.get_pc()
-             # When we pause execution with a thumb inst, we need to resume it in that mode
-             if size == 2: self.was_thumb = True
-             if size == 4: self.was_thumb = False
-             uc.emu_stop()
-             return
+            self.start_addr = self.get_pc()
+            # When we pause execution with a thumb inst, we need to resume it in that mode
+            self.was_thumb = True if size == 2 else False
+
+            if self.break_addr:
+                print "[+] Breakpoint hit! {}".format(self.break_addr)
+
+            uc.emu_stop()
+            return
 
         print out_string
 
-        # If we are stepping, we set stop_now, so next hook call we pause emulator
+        # If we are stepping, we set stop_now, so next hook call we pause emulator.
         if self.use_step_mode:
-             self.stop_now = True
+            self.stop_now = True
 
+        # Check for break points.
+        if address in self.break_points.values():
+            self.use_step_mode = True
+            self.break_addr = address
 
-        # Check for break points
         return
 
 
     def _next_bp_id(self):
         self.unique_bp_id += 1
         return self.unique_bp_id
-
